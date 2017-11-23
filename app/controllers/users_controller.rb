cc4a67f89f7d8cedc1efadb455274e093d0b1cbd
x16118219 <x16118219@student.ncirl.ie>
2017-11-23 08:27:08 +0000

    Registration Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
   
   def register
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "You have been successfully registered!"
+      redirect_to @user
+    else
+      render 'register'
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/login', to: 'static_pages#login'
   get '/register', to: 'users#register'
+  post '/register',  to: 'users#create'
   # get '/search', to: 'static_pages#search' - TODO add search non static
   # get '/cart', to: 'static_pages#cart' - TODO add cart non static
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
+  
+  resources :users
 
 end
