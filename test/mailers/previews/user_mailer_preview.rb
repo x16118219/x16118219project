66a3f4992c449a1b6c52cbddffaa0674b80c3434
x16118219 <x16118219@student.ncirl.ie>
2017-12-05 02:08:59 +0000

    retrying

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,14 +1,14 @@
-# Preview all emails at https://infinite-ravine-17937.herokuapp.com/rails/mailers/user_mailer
+# Preview all emails at https://rocky-beyond-60158.herokuapp.com/rails/mailers/user_mailer/
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at https://infinite-ravine-17937.herokuapp.com/rails/mailers/user_mailer/account_activation 
+  # Preview this email at https://rocky-beyond-60158.herokuapp.com/rails/mailers/user_mailer/account_activation 
   def account_activation
     user = User.first
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
 
-  # Preview this email at https://infinite-ravine-17937.herokuapp.com/rails/mailers/user_mailer/password_reset
+  # Preview this email at https://rocky-beyond-60158.herokuapp.com/rails/mailers/user_mailer/password_reset
   def password_reset
     user = User.first
     user.reset_token = User.new_token
