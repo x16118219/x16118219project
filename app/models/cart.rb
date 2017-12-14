8819b52d1d56ba3669fa97cf527627caae098836
x16118219 <x16118219@student.ncirl.ie>
2017-12-14 09:35:21 +0000

    colon moved is has and belongs to, only exposed on Heroku

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
-  has_and_belongs_to_many: users
-  has_and_belongs_to_many: items
+  has_and_belongs_to_many :users
+  has_and_belongs_to_many :items
   belongs_to :user, foreign_key: "user_email"  
   belongs_to :item, foreign_key: "item_title"
   
